63b3f250f81577b5b50782cc28c0cb8807238775
Kazuya Takei <myself@attakei.net>
2022-01-14 02:30:35 +0900

    feat: accept additional commit types

--- attakei_pre_commit_hooks/check_conventional_commits.py
+++ attakei_pre_commit_hooks/check_conventional_commits.py
@@ -3,9 +3,10 @@ import argparse
 import re
 import sys
 from pathlib import Path
+from typing import List
 
 
-VALID_TYPES = [
+DEFAULT_VALID_TYPES = [
     # Rule 1 and 2.
     "feat",
     "fix",
@@ -13,10 +14,11 @@ VALID_TYPES = [
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("msg_file_path", type=Path)
+parser.add_argument("--extra-types", type=str, default="", help="additional commit types as comma separated text")
+parser.add_argument("msg_file_path", type=Path, help="commit message file by commit-msg hooks")
 
 
-def validate_commit_message(src_path: Path) -> bool:
+def validate_commit_message(src_path: Path, valid_types: List[str]) -> bool:
     msg = src_path.read_text()
     rule = re.compile(
         r"(?P<type>[a-z]+)(\((?P<scope>\w+)\))?!?: (?P<desc>[^\r\n].+)(\n\n(<?P<body>.+))?"
@@ -26,15 +28,17 @@ def validate_commit_message(src_path: Path) -> bool:
         print("Invalid format for 'Conventional Commits'.")
         print("Please see https://www.conventionalcommits.org/en/v1.0.0/")
         return False
-    if matched.group("type") not in VALID_TYPES:
-        print(f"Invalid type section. select one from [{', '.join(VALID_TYPES)}]")
+    if matched.group("type") not in valid_types:
+        print(f"Invalid type section. select one from [{', '.join(valid_types)}]")
         return False
     return True
 
 
 def main():
     args = parser.parse_args()
-    result = validate_commit_message(args.msg_file_path)
+    valid_types = DEFAULT_VALID_TYPES + args.extra_types.split(",")
+    result = validate_commit_message(args.msg_file_path, valid_types)
+
     return 0 if result else 1
 
 
